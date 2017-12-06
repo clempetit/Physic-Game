eec84e7392c9cb628a4545116f147dceb8c25c66
yanisepfl <33152568+yanisepfl@users.noreply.github.com>
2017-12-06 21:02:07 +0100

    Update GameEntity.java

--- GameEntity.java
+++ GameEntity.java
@@ -1,41 +1,75 @@
-/*
- *	Author:      Clément Petit
- *	Date:        15.10.2015
- */
 
 package ch.epfl.cs107.play.game.actor;
 
+import ch.epfl.cs107.play.game.Game;
 import ch.epfl.cs107.play.math.Entity;
 import ch.epfl.cs107.play.math.EntityBuilder;
 import ch.epfl.cs107.play.math.Vector;
 
 public abstract class GameEntity {
 	private Entity entity;
-	private ActorGame actorGame;
+	private ActorGame game;
+	private EntityBuilder entityBuilder;
+	
 	
 	public GameEntity(ActorGame game, boolean fixed, Vector position) {
-		actorGame = game;
-		EntityBuilder entityBuilder = game.createEntityBuilder();
-        entityBuilder.setFixed(fixed);
-        entityBuilder.setPosition(position);
-        entity = entityBuilder.build();
+		try{
+			if(game == null|| position == null) {
+				throw new NullPointerException();
+			}
+			this.game = game;
+			entityBuilder = game.createEntityBuilder();
+			entityBuilder.setFixed(fixed);
+		    entityBuilder.setPosition(position);
+		    entity = entityBuilder.build();
+		} catch(NullPointerException e) {
+			System.out.println("Paramètre(s) indispensable(s) valant null");
+		}  
 	}
 	
 	public GameEntity(ActorGame game, boolean fixed) {
-		this(game, fixed, Vector.ZERO);
+		try {
+			if (game == null) {
+				throw new NullPointerException();
+			}
+			this.game = game;
+			entityBuilder = game.createEntityBuilder();
+			entityBuilder.setFixed(fixed);
+			entity = entityBuilder.build();
+		} catch (NullPointerException e) {
+			System.out.println("Paramètre indispensable valant null");
+		}
 	}
 	
 	public void destroy() {
         entity.destroy();
     }
 	
-	// pas sûr, return entity ou sa représentation graphique ?
+
 	protected Entity getEntity() {
 		return entity;
 	}
 	
 	protected ActorGame getOwner() {
-		return actorGame;
+		return game;
 	}
+	
+	public  boolean compare(Entity compared) {
+		if(getEntity()==compared) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public Transform getTransform() {
+		return getEntity().getTransform();
+	}
+	public Vector getVelocity() {
+		return getEntity().getVelocity();
+	}
+	
+	
 
 }
