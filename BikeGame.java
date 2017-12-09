abe1de9d584b9e149c9569208d4f8cca1b4eddee
clempetit <33152614+clempetit@users.noreply.github.com>
2017-12-09 21:28:05 +0100

    Update BikeGame.java

--- BikeGame.java
+++ BikeGame.java
@@ -1,7 +1,8 @@
 /*
- *	Author:      Yanis Berkani
- *	Date:        7 déc. 2017
+ *	Author:      Clément Petit
+ *	Date:        15.10.2015
  */
+
 package ch.epfl.cs107.play.game.actor.bike;
 
 import java.awt.Color;
@@ -29,22 +30,21 @@ public class BikeGame extends ActorGame{
 	private Finish finish;
 	private TextGraphics message;
 	private TextGraphics message1;
-	private TextGraphics message2;
-	private TextGraphics message_fixBug ; 
+	private boolean finished;
 	
 	@Override
     public boolean begin(Window window, FileSystem fileSystem) {
         super.begin(window, fileSystem);
-        
+        finished = false;
         final float crateWidth = 1.0f;
         final float crateHeight = 1.0f;
         
         /* Message vide du départ pour pallier au problème
          * de latence lors de l'arrivée du cycliste (sur mac)
          */
-        message_fixBug = new TextGraphics("", 0.3f, Color.RED, Color.WHITE, 0.02f, true, false, new Vector(0.5f, 0.5f), 1.0f, 100.0f);
-        message_fixBug.setParent(getCanvas());
-        message_fixBug.draw(getCanvas());
+        message1 = new TextGraphics("", 0.3f, Color.RED, Color.WHITE, 0.02f, true, false, new Vector(0.5f, 0.5f), 1.0f, 100.0f);
+        message1.setParent(getCanvas());
+        message1.draw(getCanvas());
         
         polyline = new Polyline(
 				-1000.0f, -1000.0f,
@@ -67,26 +67,18 @@ public class BikeGame extends ActorGame{
         crate2 = new Crate(this, false, new Vector(0.2f, 7.0f), "box.4.png", crateWidth, crateHeight, 1.0f);
         crate3 = new Crate(this, false, new Vector(2.0f, 6.0f), "box.4.png", crateWidth, crateHeight, 1.0f);
         bike = new Bike(this, false, new Vector(4.0f, 5.0f));
-        finish = new Finish(this, new Vector(10.0f, 0.0f));
+        finish = new Finish(this, new Vector(45.0f, -5.0f));
         this.setViewCandidates(bike);
         return true;
     }
     
     @Override
     public void update(float deltaTime) {
-    	 super.update(deltaTime);
-    	 
-    	 
-	 
+    	super.update(deltaTime);
+    	 	
     	 if(bike.getHit()) {
     		       bike.destroy();
-    		       message1 = new TextGraphics("", 0.3f, Color.RED, Color.BLACK, 0.02f, true, false, new Vector(0.5f, 1.8f), 1.0f, 100.0f);
-    	    	       message1.setText("PERDU !");
-    	    	 	   message1.setParent(getCanvas());
-    	    	 	   message1.setRelativeTransform(Transform.I.translated(0.0f, -1.0f));
-    	    	 	   message1.draw(getCanvas());
-    		       }  
-    	 
+    		       } 
     	 	
     	 if (this.window.getKeyboard().get(KeyEvent.VK_SPACE).isPressed()){
     	 		bike.setOppositeDirection(bike.getDirection());
@@ -110,31 +102,31 @@ public class BikeGame extends ActorGame{
     	 		bike.getRightWheel().power(0.f);
     	 		bike.getLeftWheel().power(0.f);
     	 	}
-   
-    	 
-    	
-    	 	
-
-    	 if(this.window.getKeyboard().get(KeyEvent.VK_RIGHT).isDown()) {
-    	 	if(!(finish.levelFinished())) {
-    	 		bike.getBike().applyAngularForce(-30.0f);
+   	
+    	 if (finish.getListener().hasContactWith(bike.getEntity())){
+    	    finished = true;
     	 	}
+    	 if (finished) {
+    		 message = new TextGraphics("", 0.3f, Color.RED, Color.WHITE, 0.02f, true, false, new Vector(0.5f, 0.5f), 1.0f, 100.0f);
+     	 	message.setText("BRAVO");
+     	 	message.setParent(getCanvas());
+     	 	message.setRelativeTransform(Transform.I.translated(0.0f, -1.0f));
+     	 	message.draw(getCanvas());
+     	 	bike.getRightWheel().power(0.f);
+     	 	bike.getLeftWheel().power(0.f);
+    	 }
+    	 
+    	 if(this.window.getKeyboard().get(KeyEvent.VK_RIGHT).isDown()) {
+    		 if(!(finished)) {
+     	 		bike.getBike().applyAngularForce(-30.0f);
+     	 	}
     	 }
     	 	
     	 if(this.window.getKeyboard().get(KeyEvent.VK_LEFT).isDown()) {
-    		 if(!(finish.levelFinished())){
-    	 	bike.getBike().applyAngularForce(30.0f);
-    		 }
+    		 if(!(finished)) {
+      	 		bike.getBike().applyAngularForce(30.0f);
+      	 	}
     	 }
-	    
-	 if (finish.getListener().hasContactWith(bike.getEntity())){
-    	 	message = new TextGraphics("", 0.3f, Color.RED, Color.WHITE, 0.02f, true, false, new Vector(0.5f, 0.5f), 1.0f, 100.0f);
-    	 	message.setText("BRAVO");
-    	 	message.setParent(getCanvas());
-    	 	message.setRelativeTransform(Transform.I.translated(0.0f, -1.0f));
-    	 	message.draw(getCanvas());
-    	 	}
-    	 	
     }
     
 
@@ -144,4 +136,3 @@ public class BikeGame extends ActorGame{
     
     
 }
-
