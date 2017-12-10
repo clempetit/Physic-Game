4aed81e474cd5eaf4b0d90b49793647df28211ae
clempetit <33152614+clempetit@users.noreply.github.com>
2017-12-11 00:41:58 +0100

    Update Terrain.java

--- Terrain.java
+++ Terrain.java
@@ -11,13 +11,14 @@ import ch.epfl.cs107.play.game.actor.Actor;
 import ch.epfl.cs107.play.game.actor.ActorGame;
 import ch.epfl.cs107.play.game.actor.GameEntity;
 import ch.epfl.cs107.play.game.actor.ShapeGraphics;
+import ch.epfl.cs107.play.math.Entity;
 import ch.epfl.cs107.play.math.PartBuilder;
 import ch.epfl.cs107.play.math.Polyline;
 import ch.epfl.cs107.play.window.Canvas;
 
 public class Terrain extends GameEntity implements Actor{
 	private PartBuilder partBuilder;
-	private ShapeGraphics terrain;
+	private ShapeGraphics terrainGraphics;
 	private Polyline polyline;
 	
 	public Terrain(ActorGame game, Polyline polyline, Color color1, Color color2, float friction) {
@@ -28,15 +29,20 @@ public class Terrain extends GameEntity implements Actor{
 	        partBuilder.setFriction(friction);
 	        partBuilder.build();
 	        
-	        terrain = new ShapeGraphics(polyline, color1, color2, .1f, 1.f, 0);
-	        terrain.setParent(this);
+	        terrainGraphics = new ShapeGraphics(polyline, color1, color2, .1f, 1.f, 0);
+	        terrainGraphics.setParent(this);
 	        getOwner().addActor(this);
 		
 	}
 	
 	@Override
+	public Entity getEntity() {
+		return super.getEntity();
+	}
+	
+	@Override
 	public void draw(Canvas canvas) {
-		terrain.draw(canvas);
+		terrainGraphics.draw(canvas);
 	}
 
 }
