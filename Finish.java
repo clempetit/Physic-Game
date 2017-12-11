9beab916ffb66194465fc0e412820460a76c301b
clempetit <33152614+clempetit@users.noreply.github.com>
2017-12-11 20:38:42 +0100

    Update Finish.java

--- Finish.java
+++ Finish.java
@@ -9,25 +9,23 @@ import ch.epfl.cs107.play.game.actor.Actor;
 import ch.epfl.cs107.play.game.actor.ActorGame;
 import ch.epfl.cs107.play.game.actor.GameEntity;
 import ch.epfl.cs107.play.game.actor.ImageGraphics;
+import ch.epfl.cs107.play.game.actor.Trigger;
 import ch.epfl.cs107.play.math.BasicContactListener;
 import ch.epfl.cs107.play.math.PartBuilder;
 import ch.epfl.cs107.play.math.Polygon;
 import ch.epfl.cs107.play.math.Vector;
 import ch.epfl.cs107.play.window.Canvas;
 
-public class Finish extends GameEntity implements Actor{
-	private PartBuilder partBuilder;
+public class Finish extends Trigger implements Actor{
 	private ImageGraphics FinishGraphics;
 	private Polygon polygon;
 	private final float FinishHeight = 2.0f;
 	private final float FinishWidth = 2.0f;
-	private BasicContactListener contactListener;
 	private boolean finished;
 	
 	public Finish(ActorGame game, Vector position) {
 		super(game, true, position);
 		finished = false;
-		partBuilder = getEntity().createPartBuilder();
 			
 		polygon = new Polygon(
 				0.0f, 0.0f,
@@ -36,21 +34,15 @@ public class Finish extends GameEntity implements Actor{
 				0.0f, FinishHeight
 				);
 		
-		partBuilder.setShape(polygon);
-		partBuilder.setGhost(true);
-	    partBuilder.build();
+		getPartBuilder().setShape(polygon);
+	    getPartBuilder().build();
 	        
-	    contactListener = new BasicContactListener();
-	    getEntity().addContactListener(contactListener);
-	    
 	    FinishGraphics = new ImageGraphics("flag.red.png", FinishWidth, FinishHeight);
 	    FinishGraphics.setParent(this);
         getOwner().addActor(this);
         }
 	
-	public BasicContactListener getListener() {
-		return contactListener;
-	}
+	
 	@Override
 	public void draw(Canvas canvas) {
 		FinishGraphics.draw(canvas);
