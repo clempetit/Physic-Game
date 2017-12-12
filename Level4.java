40589476c4bdfe693727909ebda950203b637555
yanisepfl <33152568+yanisepfl@users.noreply.github.com>
2017-12-12 17:12:19 +0100

    Update Level4.java

--- Level4.java
+++ Level4.java
@@ -10,6 +10,7 @@ import java.awt.Color;
 import ch.epfl.cs107.play.game.actor.ActorGame;
 import ch.epfl.cs107.play.game.actor.Level;
 import ch.epfl.cs107.play.game.actor.crate.Crate;
+import ch.epfl.cs107.play.game.actor.general.GravityWell;
 import ch.epfl.cs107.play.game.actor.general.Terrain;
 import ch.epfl.cs107.play.math.Polyline;
 import ch.epfl.cs107.play.math.Transform;
@@ -22,8 +23,12 @@ public class Level4 extends Level {
 	private Bike bike;
 	private Finish finish;
 	private Checkpoint cp;
-	private Vector bikePos = new Vector (4.0f, 5.0f);
-	private Vector cpPos = new Vector(40.0f, -5.0f);
+	private GravityWell puit1;
+	private GravityWell puit2;
+	private GravityWell puit3;
+	private GravityWell puit4;
+	private Vector bikePos = new Vector (0.0f, 0.0f);
+	private Vector cpPos = new Vector(31.0f, -5.0f);
 	
 	public Level4(ActorGame game) {
 		super(game);
@@ -34,24 +39,26 @@ public class Level4 extends Level {
         }
 		polyline = new Polyline(
 				-1000.0f, -1000.0f,
-				-1000.0f, 0.0f, 0.0f, 0.0f,
-				3.0f, 1.0f,
-				8.0f, 1.0f,
-				15.0f, 3.0f,
-				16.0f, 3.0f,
-				25.0f, 0.0f,
-				35.0f, -5.0f,
-				50.0f, -5.0f,
-				55.0f, -4.0f,
-				65.0f, 0.0f,
+				-1000.0f, 0.0f, 
+				-20.0f, 0.0f,
+				-10f, 30.0f,
+				-5.0f, 30.0f,
+				-5.0f, 0.0f,
+				15.0f, 0.0f,
+				25.0f, -5.0f,
+				150.0f, -5.0f,
 				6500.0f, -1000.0f
 				);
 		
         
 		terrain = new Terrain(getActorGame(), polyline, Color.GRAY, Color.green, 0.3f);
 		bike = new Bike(getActorGame(), false, bikePos);
-        finish = new Finish(getActorGame(), new Vector(45.0f, -5.0f));
+        finish = new Finish(getActorGame(), new Vector(140.0f, -5.0f));
         cp = new Checkpoint(getActorGame(), cpPos);
+        puit1 = new GravityWell(getActorGame(), true, new Vector(35.0f, -5.0f), new Vector(-10.0f, 40.0f), 0.1f, 8.0f, 15.0f);
+        puit2 = new GravityWell(getActorGame(), true, new Vector(43.0f, -5.0f), new Vector(0.0f, -30.0f), 0.1f, 8.0f, 15.0f);
+        puit3 = new GravityWell(getActorGame(), true, new Vector(35.0f, -5.0f), new Vector(30.0f, 0.0f), 0.1f, 8.0f, 15.0f);
+        puit4 = new GravityWell(getActorGame(), true, new Vector(43.0f, -5.0f), new Vector(0.0f, -30.0f), 0.1f, 8.0f, 15.0f);
         getActorGame().setViewCandidates(bike);
 	}
 	
@@ -68,6 +75,11 @@ public class Level4 extends Level {
 	@Override
 	public Checkpoint getCp() {
 		return cp;
+	}
+	
+	public void update(float deltaTime) {
+		puit1.gravityAction(bike.getBikeEntity());
+		puit2.gravityAction(bike.getBikeEntity());
 	}
 	
 	@Override
