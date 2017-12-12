5605e2439af14829da81cf0a366812cc0bdee6f8
clempetit <33152614+clempetit@users.noreply.github.com>
2017-12-12 02:49:38 +0100

    Update Level3.java

--- Level3.java
+++ Level3.java
@@ -9,11 +9,9 @@ package ch.epfl.cs107.play.game.actor.bike;
 import java.awt.Color;
 
 import ch.epfl.cs107.play.game.actor.ActorGame;
-import ch.epfl.cs107.play.game.actor.ImageGraphics;
 import ch.epfl.cs107.play.game.actor.Level;
-import ch.epfl.cs107.play.game.actor.ShapeGraphics;
+import ch.epfl.cs107.play.game.actor.general.GravityWell;
 import ch.epfl.cs107.play.game.actor.general.Terrain;
-import ch.epfl.cs107.play.math.Polygon;
 import ch.epfl.cs107.play.math.Polyline;
 import ch.epfl.cs107.play.math.Transform;
 import ch.epfl.cs107.play.math.Vector;
@@ -24,32 +22,10 @@ public class Level3 extends Level {
 	private Terrain terrain;
 	private Bike bike;
 	private Finish finish;
-	private Puits puit1;
-	private Puits puit2;
-	private boolean insidePuit1;
-	private boolean insidePuit2;
+	private GravityWell puit;
 	
 	public Level3(ActorGame game) {
 		super(game);
-		
-		
-		
-		
-	}
-	
-	public Puits getPuit1() {
-		return puit1;
-	}
-	
-	public Puits getPuit2() {
-		return puit2;
-	}
-	
-	public boolean getInsidePuit1() {
-		return insidePuit1;
-	}
-	public boolean getInsidePuit2() {
-		return insidePuit2;
 	}
 	
 	public void createAllActors() {
@@ -60,29 +36,28 @@ public class Level3 extends Level {
 				-20.0f, 20.0f,
 				-20.0f, 0.f,
 				0.0f, 0.0f,
-				100.0f, 0.0f,
+				39.0f, 0.0f,
+				39.0f, 4.0f,
+				50.0f, 4.0f,
 				6500.0f, -1000.0f
 				);
 		
 		
 		terrain = new Terrain(getActorGame(), polyline, Color.GRAY, Color.green, 1.0f);
-		bike = new Bike(getActorGame(), false, new Vector(0.0f, 0.0f));
-        finish = new Finish(getActorGame(), new Vector(40.0f, 0.0f));
-        puit1 = new Puits(getActorGame(), true, new Vector(25.0f, 0.0f));
-        puit2 = new Puits(getActorGame(), true, new Vector(30.0f, 0.0f));
+		bike = new Bike(getActorGame(), false, new Vector(0.0f, 3.0f));
+        finish = new Finish(getActorGame(), new Vector(49.0f, 4.0f));
+        puit = new GravityWell(getActorGame(), true, new Vector(25.0f, 0.0f), new Vector(0.0f, 50.0f));
+        
+        
+
         getActorGame().setViewCandidates(bike);
+        
+        
 	}
 	
-	public void changeGravity() {
-		if(puit1.hasContactWith(bike.getBikeEntity()) || puit1.hasContactWith(bike.getLeftWheel().getEntity())|| puit1.hasContactWith(bike.getRightWheel().getEntity())) {
-			bike.getBikeEntity().destroy();
-		}
-		
-		if(puit2.hasContactWith(bike.getBikeEntity()) || puit2.hasContactWith(bike.getLeftWheel().getEntity())|| puit2.hasContactWith(bike.getRightWheel().getEntity())) {
-			bike.getBikeEntity().applyAngularForce(10.0f );
-		} 
+	public void update(float deltaTime) {
+		puit.gravityAction(bike.getBikeEntity());
 	}
-	
 	@Override
 	public Finish getFinish() {
 		return finish;
@@ -97,6 +72,7 @@ public class Level3 extends Level {
 	public void draw(Canvas canvas) {
 		terrain.draw(canvas);
 		finish.draw(canvas);
+		puit.draw(canvas);
 	}
 	
 	//la redéfinition de getTransform() et getVelocity() est obligatoire ici comme dans chaque autre level,
